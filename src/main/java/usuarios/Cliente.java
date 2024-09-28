f2eb866ff86f2db4b70aea7737e98dd234942f9a
Franco Dellarupe <fran.dellarupe03@gmail.com>
2024-09-28 12:57:34 -0300

    Agrega modelos de usuarios

---
+++ src/main/java/usuarios/Cliente.java
@@ -0,0 +1,19 @@
+package usuarios;
+
+public class Cliente extends NoAdmin {
+
+	private String email;
+
+	public Cliente() {
+		super();
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
---
+++ src/main/java/usuarios/NoAdmin.java
@@ -0,0 +1,37 @@
+package usuarios;
+
+public abstract class NoAdmin extends Usuario {
+	
+	private String nombre;
+	private String apellido;
+	private String urlImage;
+	
+	public NoAdmin() {
+		super();
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getApellido() {
+		return apellido;
+	}
+	
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	public String getUrlImage() {
+		return urlImage;
+	}
+	
+	public void setUrlImage(String urlImage) {
+		this.urlImage = urlImage;
+	}
+	
+}
