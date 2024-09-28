3d81453d984260a6d19b4d488e24d5aa9e19cbbf
Pedro0604 <pedrospadari0604@gmail.com>
2024-09-28 15:23:25 -0300

    Corrige constructor

--- src/main/java/usuarios/ResponsableDeTurno.java
+++ src/main/java/usuarios/ResponsableDeTurno.java
@@ -4,8 +4,9 @@ public class ResponsableDeTurno extends NoAdmin {
 
     private Turno turno;
 
-    public ResponsableDeTurno(int dni, String clave, String nombre, String apellido, String urlImagen) {
+    public ResponsableDeTurno(int dni, String clave, String nombre, String apellido, String urlImagen, Turno turno) {
         super(dni, clave, nombre, apellido, urlImagen);
+        this.turno = turno;
     }
 
     public Turno getTurno() {
