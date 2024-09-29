6f310fe5acc313f3f3eb9ee9d016ab19ce1495c9
Pedro0604 <pedrospadari0604@gmail.com>
2024-09-29 12:08:21 -0300

    Modifica el nombre de variable precio a precioUnitario

--- src/main/java/menu/ItemPedido.java
+++ src/main/java/menu/ItemPedido.java
@@ -1,23 +1,23 @@
 package menu;
 
 public class ItemPedido {
-    private double precio;
+    private double precioUnitario;
     private Preciable item;
     private int cantidad;
 
     public ItemPedido(Preciable item) {
         // Guardamos el precio para que aunque cambie el precio del item, el precio del pedido no cambie
-        this.precio = item.getPrecio();
+        this.precioUnitario = item.getPrecio();
         this.item = item;
         this.cantidad = 0;
     }
 
     public double getPrecioUnitario() {
-        return precio;
+        return precioUnitario;
     }
 
     public double getPrecio() {
-        return precio * cantidad;
+        return precioUnitario * cantidad;
     }
 
     public Preciable getItem() {
