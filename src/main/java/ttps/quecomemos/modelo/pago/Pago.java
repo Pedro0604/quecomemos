74ac3873f61e3a0fedc1935d9688b7f96417ac29
Gregorio Ponce <gregorio.ponce213612@alumnos.info.unlp.edu.ar>
2024-10-23 15:35:57 -0300

    Pago listo

--- src/main/java/ttps/quecomemos/modelo/pago/Pago.java
+++ src/main/java/ttps/quecomemos/modelo/pago/Pago.java
@@ -1,22 +1,50 @@
 package ttps.quecomemos.modelo.pago;
 
+import jakarta.persistence.*;
 import ttps.quecomemos.modelo.pedido.Pedido;
 
+@Entity
 public class Pago {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pedido_id", nullable = false)
     private Pedido pedido;
+
+    @Enumerated(EnumType.STRING) //
+    @Column(nullable = false)
     private MetodoPago metodo;
+
+
+    public Pago() {
+    }
 
     public Pago(Pedido pedido, MetodoPago metodo) {
         this.pedido = pedido;
         this.metodo = metodo;
     }
 
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
     public Pedido getPedido() {
         return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
     }
 
     public MetodoPago getMetodo() {
         return metodo;
     }
 
+    public void setMetodo(MetodoPago metodo) {
+        this.metodo = metodo;
+    }
 }
