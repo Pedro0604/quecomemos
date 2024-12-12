866dd7e3935fdbad2230744bba53b0d2cad42d16
Pedro0604 <pedrospadari0604@gmail.com>
2024-12-12 20:38:13 -0300

    Controller, service y request de MenuDiario

---
+++ src/main/java/ttps/quecomemos/controller/MenuDiarioController.java
@@ -0,0 +1,74 @@
+package ttps.quecomemos.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ttps.quecomemos.dto.MenuDiarioRequest;
+import ttps.quecomemos.modelo.menu.MenuDiario;
+import ttps.quecomemos.service.menu.MenuDiarioService;
+import ttps.quecomemos.util.JwtUtil;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping(path = "/menus-diarios", produces = MediaType.APPLICATION_JSON_VALUE)
+
+public class MenuDiarioController {
+
+    private final MenuDiarioService menuDiarioService;
+    private final JwtUtil jwtUtil = new JwtUtil();
+
+    @Autowired
+    public MenuDiarioController(MenuDiarioService menuDiarioService) {
+        this.menuDiarioService = menuDiarioService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MenuDiario>> getAllMenusDiarios(@RequestHeader("Authorization") String token) {
+        if (token != null && jwtUtil.validateToken(token.replace("Bearer ", ""))) {
+            return ResponseEntity.ok(menuDiarioService.findAll());
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MenuDiario> getMenuDiario(@PathVariable Long id, @RequestHeader("Authorization") String token) {
+        if (token != null && jwtUtil.validateToken(token.replace("Bearer ", ""))) {
+            return ResponseEntity.ok(menuDiarioService.findById(id));
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<MenuDiario> createMenuDiario(@RequestBody MenuDiarioRequest menuDiarioRequest, @RequestHeader("Authorization") String token) {
+        if (token != null && jwtUtil.validateToken(token.replace("Bearer ", ""))) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(menuDiarioService.saveMenuDiarioRequest(menuDiarioRequest));
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MenuDiario> updateMenuDiario(@PathVariable Long id, @RequestBody MenuDiarioRequest menuDiarioRequest, @RequestHeader("Authorization") String token) {
+        if (token != null && jwtUtil.validateToken(token.replace("Bearer ", ""))) {
+            return ResponseEntity.ok(menuDiarioService.updateMenuDiarioRequest(menuDiarioRequest, id));
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMenuDiario(@PathVariable Long id, @RequestHeader("Authorization") String token) {
+        if (token != null && jwtUtil.validateToken(token.replace("Bearer ", ""))) {
+            menuDiarioService.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+}
--- src/main/java/ttps/quecomemos/service/menu/MenuDiarioService.java
+++ src/main/java/ttps/quecomemos/service/menu/MenuDiarioService.java
@@ -2,14 +2,59 @@ package ttps.quecomemos.service.menu;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ttps.quecomemos.dto.MenuDiarioRequest;
+import ttps.quecomemos.modelo.menu.Menu;
 import ttps.quecomemos.modelo.menu.MenuDiario;
 import ttps.quecomemos.repository.menu.MenuDiarioRepository;
 import ttps.quecomemos.service.GenericService;
 
+// Las validaciones sobre los datos de los menús las realiza la clase
 @Service
 public class MenuDiarioService extends GenericService<MenuDiario> {
+    private final MenuService menuService;
+
     @Autowired
-    public MenuDiarioService(MenuDiarioRepository menuDiarioRepository) {
+    public MenuDiarioService(MenuDiarioRepository menuDiarioRepository, MenuService menuService) {
         super(menuDiarioRepository);
+        this.menuService = menuService;
+    }
+
+    private MenuDiario createMenuDiarioFromRequest(MenuDiarioRequest menuDiarioRequest) {
+        MenuDiario menuDiario = new MenuDiario(menuDiarioRequest);
+
+        Menu menuVegetariano = menuService.findById(menuDiarioRequest.getMenuVegetarianoId());
+        Menu menuNoVegetariano = menuService.findById(menuDiarioRequest.getMenuNoVegetarianoId());
+        menuDiario.setMenuNoVegetariano(menuNoVegetariano);
+        menuDiario.setMenuVegetariano(menuVegetariano);
+
+        return menuDiario;
+    }
+
+    @Transactional
+    @Override
+    public MenuDiario save(MenuDiario menuDiario) {
+        return this.repository.save(menuDiario);
+    }
+
+    @Transactional
+    public MenuDiario saveMenuDiarioRequest(MenuDiarioRequest menuDiarioRequest) {
+        return this.repository.save(this.createMenuDiarioFromRequest(menuDiarioRequest));
+    }
+
+    @Transactional
+    @Override
+    public MenuDiario update(MenuDiario menuDiario, Long id) {
+        findById(id); // Si el menú que se intenta actualizar no existe lanza una excepción
+        menuDiario.setId(id);
+        return this.repository.save(menuDiario);
+    }
+
+    @Transactional
+    public MenuDiario updateMenuDiarioRequest(MenuDiarioRequest menuDiarioRequest, Long id) {
+        findById(id); // Si el menú que se intenta actualizar no existe lanza una excepción
+        MenuDiario menuDiario = this.createMenuDiarioFromRequest(menuDiarioRequest);
+        menuDiario.setId(id);
+        return this.repository.save(menuDiario);
     }
 }
